a99ea0ed574743117ba59428769202b0fdc01891
cedoduarte <carlosduarte.1@hotmail.com>
2022-09-03 22:12:27 -0700

    first

---
+++ src/app/components/additem/additem.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ItemService } from 'src/app/services/item.service';
+import { Item } from "../../models/item"
+
+@Component({
+  selector: 'app-additem',
+  templateUrl: './additem.component.html',
+  styleUrls: ['./additem.component.css']
+})
+export class AddItemComponent implements OnInit {
+  
+  id: number = 0;
+  title: string = "";
+  price: number = 0;
+  quantity: number = 0;
+  
+  // variables injection here in constructor
+  constructor(private itemService: ItemService, private router: Router) {
+
+  }
+  
+  ngOnInit() {
+  }
+  
+  onSubmit() {
+    const item = new Item();
+    item.id = this.id;
+    item.title = this.title;
+    item.price = this.price;
+    item.quantity = this.quantity;
+    item.completed = false;
+
+    //this.itemService.addItem(item);
+    this.itemService.addItem(item).subscribe(i => {
+      this.router.navigate(["/"]);
+    });
+  }  
+}
